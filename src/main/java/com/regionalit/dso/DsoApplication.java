895815d3bd2535e9e8e86e5d11aee8c693cbd3c5
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-01-22 23:23:20 +0800

    [xiaojun] Init spring boot project

---
+++ src/main/java/com/regionalit/dso/DsoApplication.java
@@ -0,0 +1,20 @@
+package com.regionalit.dso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DsoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DsoApplication.class, args);
+  }
+
+  @GetMapping("")
+  String hello() {
+    return "hello xiaojun!";
+  }
+}
---
+++ src/test/java/com/regionalit/dso/DsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regionalit.dso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
